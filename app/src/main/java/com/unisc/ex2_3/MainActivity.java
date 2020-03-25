a723e847cdb4025354692540cee6591b8e14604c
gabrielquadro <61526044+gabrielquadro@users.noreply.github.com>
2020-03-25 18:03:22 -0300

    Alterações de tamanho, adição do scroll view, ícone na barra

--- app/src/main/java/com/unisc/ex2_3/MainActivity.java
+++ app/src/main/java/com/unisc/ex2_3/MainActivity.java
@@ -2,6 +2,8 @@ package com.unisc.ex2_3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setIcon(R.drawable.quiz_transito_icon);
+        getSupportActionBar().setBackgroundDrawable(
+                new ColorDrawable(Color.parseColor("#000000")));
+
+        this.setTitle(getResources().getString(R.string.titulo));
+
+
     }
 }
